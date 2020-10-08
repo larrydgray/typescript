ac42b26fd381b39147fc4147abe430d78f61d162
caverdude <javacaverdude@gmail.com>
2020-10-08 17:08:54 -0500

    chapter 3

--- tschap3/primer/index.js
+++ tschap3/primer/index.js
@@ -1,7 +1,8 @@
 let hatPrice = 100;
 console.log(`Hat price: ${hatPrice}`);
 let bootPrice = "100";
-console.log(`Boot Pricce: ${bootPrice}`);
+console.log(`Boot Price: ${bootPrice}`);
+
 if(hatPrice === bootPrice){
     console.log("Prices are the same");
 } else {
@@ -15,3 +16,8 @@ let myVariable= "Adam";
 console.log(`Type: ${typeof myVariable}`);
 myVariable= 100;
 console.log(`Type: ${typeof myVariable}`);
+
+//fallback example
+let firstCity;
+let secondCity = firstCity || "London";
+console.log(`City: ${ secondCity}`);
