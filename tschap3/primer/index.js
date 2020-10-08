bbd8e9b44efaf35223271cc6b195a78e0397ee20
caverdude <javacaverdude@gmail.com>
2020-10-08 18:07:53 -0500

    chp3 arrays

--- tschap3/primer/index.js
+++ tschap3/primer/index.js
@@ -1,38 +1,15 @@
-let hatPrice = 100;
-console.log(`Hat price: ${hatPrice}`);
-let bootsPrice = "100";
-console.log(`Boot Price: ${bootsPrice}`);
+let names  = ["Hat", "Boots", "Gloves"];
+let prices = [];
+
+prices.push(100);
+prices.push("100");
+prices.push(50.25);
+
+console.log(`First Item: ${names[0]}: ${prices[0]}`);
 
 let sumPrices=(...numbers) => numbers.reduce((total, val) =>
         total+(Number.isNaN(Number(val))?0:Number(val)));
 
+let totalPrice = sumPrices(...prices);
+console.log(`Total: ${totalPrice} ${typeof totalPrice}`);
 
-
-let totalPrice = sumPrices(hatPrice, bootsPrice);
-console.log(`Total Price: ${totalPrice} ${typeof totalPrice}`);
-
-totalPrice = sumPrices(100, 200, 300);
-console.log(`Total Price: ${totalPrice} ${typeof totalPrice}`);
-
-totalPrice = sumPrices(100, 200, undefined, false, "hello");
-console.log(`Total Price: ${totalPrice} ${typeof totalPrice}`);
-
-
-if(hatPrice === bootsPrice){
-    console.log("Prices are the same");
-} else {
-    console.log("Prices are different");
-}
-
-totalPrice = Number(hatPrice) + Number(bootsPrice);
-console.log(`Total Price: ${totalPrice}`);
-
-let myVariable= "Adam";
-console.log(`Type: ${typeof myVariable}`);
-myVariable= 100;
-console.log(`Type: ${typeof myVariable}`);
-
-//fallback example
-let firstCity;
-let secondCity = firstCity || "London";
-console.log(`City: ${ secondCity}`);
