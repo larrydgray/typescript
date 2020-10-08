9c493e641032d4d55b43d419e592e1e6c0d6df50
caverdude <javacaverdude@gmail.com>
2020-10-08 16:59:15 -0500

    chapter 3

--- tschap3/primer/index.js
+++ tschap3/primer/index.js
@@ -2,11 +2,16 @@ let hatPrice = 100;
 console.log(`Hat price: ${hatPrice}`);
 let bootPrice = "100";
 console.log(`Boot Pricce: ${bootPrice}`);
-if(hatPrice == bootPrice){
+if(hatPrice === bootPrice){
     console.log("Prices are the same");
 } else {
     console.log("Prices are different");
 }
 
-let totalPrice = hatPrice + bootPrice;
+let totalPrice = Number(hatPrice) + Number(bootPrice);
 console.log(`Total Price: ${totalPrice}`);
+
+let myVariable= "Adam";
+console.log(`Type: ${typeof myVariable}`);
+myVariable= 100;
+console.log(`Type: ${typeof myVariable}`);
