8815acd1356cb01a1b46cc2c925d772badaa5787
caverdude <javacaverdude@gmail.com>
2020-10-08 17:46:46 -0500

    chapter 3

--- tschap3/primer/index.js
+++ tschap3/primer/index.js
@@ -4,9 +4,8 @@ let bootsPrice = "100";
 console.log(`Boot Price: ${bootsPrice}`);
 
 function sumPrices(...numbers){
-    return numbers.reduce(function (total, val) {
-        return total+(Number.isNaN(Number(val))?0:Number(val));
-    }, 0);
+    return numbers.reduce((total, val) =>
+        total+(Number.isNaN(Number(val))?0:Number(val)));
 }
 
 
