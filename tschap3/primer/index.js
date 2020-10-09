43e7ae2cd8f2efe67f6981180524ec0a3359d234
caverdude <javacaverdude@gmail.com>
2020-10-09 11:37:56 -0500

    chp3 objects this keyword

--- tschap3/primer/index.js
+++ tschap3/primer/index.js
@@ -14,11 +14,15 @@ let myObject = {
 function writeMessage(message) {
     console.log(`${this.greeting}, ${message}`);
 }
+
+myObject.writeMessage = myObject.writeMessage.bind(myObject);
+
 greeting = "Hello";
 
 myObject.writeMessage("It is sunny today");
 let myFunction = myObject.writeMessage;
 myFunction("It is sunny today!");
+
 
 
 writeMessage("It is sunny today");
