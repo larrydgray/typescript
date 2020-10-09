7577b207253a2f18a7c08d36f045fe3252174419
caverdude <javacaverdude@gmail.com>
2020-10-08 20:24:36 -0500

    chp3 objects

--- tschap3/primer/index.js
+++ tschap3/primer/index.js
@@ -19,33 +19,10 @@ let boots = {
     }
 }
 
-let gloves = {
-    productName:"Gloves",
-    price:40
-}
+console.log(`Hat: ${hat.price}, ${hat.priceIncTax}`);
+hat.price=120;
+console.log(`Hat: ${hat.price}, ${hat.priceIncTax}`);
 
-gloves.name = gloves.productName;
-delete gloves.productName;
-gloves.price=20;
-
-let sumPrices=(...numbers) => numbers.reduce((total, val) =>
-        total+(Number.isNaN(Number(val))?0:Number(val)));
-
-let totalPrice = sumPrices(hat.price, boots.price, gloves.price);
-console.log(`Total: ${totalPrice} ${typeof totalPrice}`);
-
-let propertyCheck = hat.price|| 0;
-let objectAndPropertyCheck = (hat || {}).price || 0;
-console.log(`Checks: ${propertyCheck}, ${objectAndPropertyCheck}`);
-
-let otherHat = { ...hat};
-console.log(`Spread: ${otherHat.name}, ${otherHat.price}`);
-
-let additionalProperties = { ...hat, discounted: true};
-console.log(`Additional: ${JSON.stringify(additionalProperties)}`);
-
-let replacedProperties = { ...hat, price: 10};
-console.log(`Replaced: ${JSON.stringify(replacedProperties)}`);
-
-let { price, ...someProperties } = hat;
-console.log(`Selected: ${JSON.stringify(someProperties)}`);
+console.log(`Boots: ${boots.price}, ${boots.priceIncTax}`);
+boots.price="120";
+console.log(`Boots: ${boots.price}, ${boots.priceIncTax}`);
