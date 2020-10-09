9f16313a30e2d6dbf6a9e201f7941eae1a223113
caverdude <javacaverdude@gmail.com>
2020-10-08 20:50:21 -0500

    chp3 objects this keyword

--- tschap3/primer/index.js
+++ tschap3/primer/index.js
@@ -1,36 +1,5 @@
-let hat={
-    name: "Hat",
-    _price: 100,
-    priceIncTax: 100*1.2,
-    set price(newPrice){
-        this._price=newPrice;
-        this.priceIncTax = this._price*1.2;
-    },
-    get price(){
-        return this._price;
-    },
-    writeDetails: () =>
-        console.log(`${this.name}: ${this.price}, ${this.priceIncTax}`)
-
-
-};
-
-let boots = {
-    name:"Boots",
-    price:"100",
-    get priceIncTax(){
-        return Number(this.price)*1.2;
-    }
+function writeMessage(message) {
+    console.log(`${this.greeting}, ${message}`);
 }
-
-hat.writeDetails();
-hat.price = 120;
-hat.writeDetails();
-console.log('------');
-console.log(`Hat: ${hat.price}, ${hat.priceIncTax}`);
-hat.price=120;
-console.log(`Hat: ${hat.price}, ${hat.priceIncTax}`);
-
-console.log(`Boots: ${boots.price}, ${boots.priceIncTax}`);
-boots.price="120";
-console.log(`Boots: ${boots.price}, ${boots.priceIncTax}`);
+greeting = "Hello";
+writeMessage("It is sunny today");
