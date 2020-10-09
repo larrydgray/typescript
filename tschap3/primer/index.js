1547952d110cf3053c99e1272b793faba354df84
caverdude <javacaverdude@gmail.com>
2020-10-08 20:19:28 -0500

    chp3 objects

--- tschap3/primer/index.js
+++ tschap3/primer/index.js
@@ -1,11 +1,22 @@
 let hat={
     name: "Hat",
-    price: 100
+    _price: 100,
+    priceIncTax: 100*1.2,
+    set price(newPrice){
+        this._price=newPrice;
+        this.priceIncTax = this._price*1.2;
+    },
+    get price(){
+        return this._price;
+    }
 };
 
 let boots = {
     name:"Boots",
-    price:"100"
+    price:"100",
+    get priceIncTax(){
+        return Number(this.price)*1.2;
+    }
 }
 
 let gloves = {
@@ -30,3 +41,11 @@ console.log(`Checks: ${propertyCheck}, ${objectAndPropertyCheck}`);
 let otherHat = { ...hat};
 console.log(`Spread: ${otherHat.name}, ${otherHat.price}`);
 
+let additionalProperties = { ...hat, discounted: true};
+console.log(`Additional: ${JSON.stringify(additionalProperties)}`);
+
+let replacedProperties = { ...hat, price: 10};
+console.log(`Replaced: ${JSON.stringify(replacedProperties)}`);
+
+let { price, ...someProperties } = hat;
+console.log(`Selected: ${JSON.stringify(someProperties)}`);
