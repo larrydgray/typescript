3d9080493f989bd926d2bea9b9dabf5b8d731dc9
caverdude <javacaverdude@gmail.com>
2020-10-12 17:46:58 -0500

    chp4 more JavaScript OOP generator Map

--- tschap4/index.js
+++ tschap4/index.js
@@ -7,33 +7,12 @@ class Product {
         return `toString: Name: ${this.name}, Price: ${this.price}`;
     }
 }
+let data = new Map();
 
-class GiftPack {
-    constructor(name, prod1, prod2, prod3) {
-        this.name = name;
-        this.prod1 = prod1;
-        this.prod2 = prod2;
-        this.prod3 = prod3;
+data.set("hat",  new Product("Hat", 100));
+data.set("boots", new Product("Boots",100));
 
-    }
-    getTotalPrice(){
-        return [this.prod1, this.prod2, this.prod3]
-            .reduce((total, p)=> total+p.price, 0);
-    }
-    *[Symbol.iterator](){
-        yield this.prod1;
-        yield this.prod2;
-        yield this.prod3;
-    }
-}
-
-let winter = new GiftPack("winter", new Product("Hat", 100),
-    new Product("Boots", 80), new Product("Gloves", 23));
-
-console.log(`Total price: ${winter.getTotalPrice()}`);
-
-[...winter].forEach(p => console.log(`Product:${p}`));
-
+[...data.keys()].forEach(key => console.log(data.get(key).toString()));
 
 
 
