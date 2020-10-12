96e3e7eec231bfd10a31e30178340662c8efe20f
caverdude <javacaverdude@gmail.com>
2020-10-12 16:01:39 -0500

    chp4 more JavaScript OOP class

--- tschap4/index.js
+++ tschap4/index.js
@@ -7,9 +7,26 @@ class Product {
         return `toString: Name: ${this.name}, Price: ${this.price}`;
     }
 }
+class TaxedProduct extends Product {
 
-let hat = new Product("Hat", 100);
-let boots = new Product("Boots", 100);
+    constructor(name, price, taxRate = 1.2) {
+        super(name, price);
+        this.taxRate = taxRate;
+    }
+
+    getPriceIncTax(){
+        return Number(this.price) * this.taxRate;
+    }
+
+    toString() {
+        let chainResult = super.toString();
+        return `${chainResult}, Tax: ${this.getPriceIncTax()}`;
+    }
+
+}
+
+let hat = new TaxedProduct("Hat", 100);
+let boots = new TaxedProduct("Boots", 100, 1.3);
 
 console.log(hat.toString());
 console.log(boots.toString());
