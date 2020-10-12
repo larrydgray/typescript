fe6a22719917d599c7468fa17ee4f1587ff3712e
caverdude <javacaverdude@gmail.com>
2020-10-12 16:39:25 -0500

    chp4 more JavaScript OOP iterator doesnt work

--- tschap4/index.js
+++ tschap4/index.js
@@ -7,29 +7,40 @@ class Product {
         return `toString: Name: ${this.name}, Price: ${this.price}`;
     }
 }
-class TaxedProduct extends Product {
 
-    constructor(name, price, taxRate = 1.2) {
-        super(name, price);
-        this.taxRate = taxRate;
-    }
 
-    getPriceIncTax(){
-        return Number(this.price) * this.taxRate;
-    }
+function createProductIterator() {
+    const hat = new Product("Hat", 100);
+    const boots = new Product("Boots", 100);
+    const umbrella = new Product("Umbrella", 23);
 
-    toString() {
-        let chainResult = super.toString();
-        return `${chainResult}, Tax: ${this.getPriceIncTax()}`;
-    }
-    static process(...products) {
-        products.forEach(p =>console.log(p.toString()));
-    }
+    let lastVal;
 
+    return {
+        next(){
+            switch(lastVal){
+                case undefined:
+                    lastVal = hat;
+                    return { value: hat, done: false};
+                case hat:
+                    lastVal = boots;
+                    return { value: boots, done: false};
+                case boots:
+                    lastVal = umbrella;
+                    return { value: umbrella,done: false};
+                case umbrella:
+                    return {value: undefined, done: true};
+            }
+        }
+    }
 }
-TaxedProduct.process(new TaxedProduct("Hat", 100, 1.2),
-    new TaxedProduct("Boots", 100));
 
+let iterator = createProductIterator();
+let result = iterator.next();
+while(!result.done){
+    console.log(result.value.toString());
+    result.iterator.next();
+}
 
 
 
