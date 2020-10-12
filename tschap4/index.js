53f600348c2c25b4d428d875fd8dd6389eaa69b5
caverdude <javacaverdude@gmail.com>
2020-10-12 15:34:18 -0500

    chp4 more JavaScript OOP prototype

--- tschap4/index.js
+++ tschap4/index.js
@@ -6,32 +6,13 @@ Product.prototype.toString = function(){
     return `toString: Name: ${this.name}, Price: ${this.price}`;
 }
 
-let TaxedProduct = function (name, price, taxRate){
-    Product.call(this,name, price);
-    this.taxRate = taxRate;
-}
+Product.process = (...products) =>
+    products.forEach(p => console.log(p.toString()));
+Product.process(new Product("Hat", 100, 1.2), new Product("Boots",100));
 
-Object.setPrototypeOf( TaxedProduct.prototype, Product.prototype);
 
-TaxedProduct.prototype.getPriceIncTax = function(){
-    return Number(this.price) * this.taxRate;
-}
 
-TaxedProduct.prototype.toString = function(){
-    let chainResult = Product.prototype.toString.call(this);
-    return `${chainResult}, Tax: ${this.getPriceIncTax()}`;
-}
 
-let hat = new TaxedProduct("Hat", 100, 1.2);
-let boots = new Product("Boots", 100);
-
-console.log(hat.toString());
-console.log(boots.toString());
-
-console.log(`hat and TaxedProduct: ${ hat instanceof TaxedProduct}`);
-console.log(`hat and Product: ${ hat instanceof Product}`);
-console.log(`boots and TaxedProduct: ${boots instanceof TaxedProduct}`);
-console.log(`boots and Product: ${boots instanceof Product}`);
 
 
 
