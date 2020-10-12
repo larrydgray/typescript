2b0b8c73598d89d6da618d0a6aff5dbcc1d22530
caverdude <javacaverdude@gmail.com>
2020-10-12 13:51:51 -0500

    chp4 more JavaScript OOP prototype

--- tschap4/index.js
+++ tschap4/index.js
@@ -5,6 +5,19 @@ let hat = {
         return Number(this.price) * 1.2;
     }
 };
+let boots = {
+    name: "Boots",
+    price: 100,
+    getPriceIncIncTax(){
+        return Number(this.price) * 1.2;
+    }
+}
 
-console.log(`Hat: ${hat.price}, ${hat.getPriceIncIncTax()}`);
-console.log(`toString: ${hat.toString()}`);
+let hatPrototype = Object.getPrototypeOf(hat);
+console.log(`Hat Prototype: ${hatPrototype}`);
+
+let bootsPrototype =  Object.getPrototypeOf(boots);
+console.log(`Boots Prototype: ${bootsPrototype}`);
+
+console.log(`Common prototype: ${ hatPrototype === bootsPrototype}`);
+
