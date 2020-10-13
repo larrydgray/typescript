7be960a811da99146a9926bd41487a2e642d9df9
caverdude <javacaverdude@gmail.com>
2020-10-12 20:25:34 -0500

    chp4 more JavaScript modules

--- tschap4/index.js
+++ tschap4/index.js
@@ -1,4 +1,4 @@
-import {calculateTax} from "./tax";
+import calcTaxAndSum, {calculateTax} from "./tax";
 
 class Product {
     constructor(name, price) {
@@ -13,8 +13,11 @@ class Product {
 
 let product = new Product("Hat", 100);
 let taxedPrice = calculateTax(product.price);
-
 console.log(`Name: ${product.name}, Taxed Price: ${taxedPrice}`);
+
+let products = [new Product("Gloves", 23), new Product("Boots", 100)];
+let totalPrice = calcTaxAndSum(...products.map(p => p.price));
+console.log(`Total Price:${totalPrice.toFixed(2)}`);
 
 
 
