99b51252d10eb1b191cf32c96dff2ca8e5418603
caverdude <javacaverdude@gmail.com>
2020-10-13 18:39:11 -0500

    chp5 compiler

--- tschap5/tools/dist/index.js
+++ tschap5/tools/dist/index.js
@@ -3,4 +3,7 @@ var printMessage = function (msg) {
 };
 var message = ("Hello, TypeScript");
 printMessage(message);
-//printMessage(100);
+var data = new Map();
+data.set("Bob", "London");
+data.set("Alice", "Paris");
+data.forEach(function (val, key) { return console.log(key + " lives in " + val); });
---
+++ tschap5/tools/readme.txt
@@ -0,0 +1,2 @@
+You will se only one real file here index.js and somme config files, the code in this case for this chapter
+is in each section pushed to github or in other words revision history.
