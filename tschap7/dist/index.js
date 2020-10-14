26d9187d00ee380cd21539a600391ef0f413f3da
caverdude <javacaverdude@gmail.com>
2020-10-14 18:50:25 -0500

    Chapter7 Static types

--- tschap7/dist/index.js
+++ tschap7/dist/index.js
@@ -2,9 +2,10 @@ function calculateTax(amount, format) {
     const calcAmount = amount * 1.2;
     return format ? `$${(amount * 1.2).toFixed(2)}` : calcAmount;
 }
-let taxNumber = calculateTax(100, false);
-let taxString = calculateTax(100, true);
-let taxBoolean = calculateTax(100, false);
-console.log(`Number value: ${taxNumber.toFixed(2)}`);
-console.log(`String value: ${taxString.charAt(0)}`);
-console.log(`Boolean Value: ${taxBoolean}`);
+let taxValue = calculateTax(100, false);
+if (typeof taxValue === "number") {
+    console.log(`Number Value: ${taxValue.toFixed(2)}`);
+}
+else if (typeof taxValue === "string") {
+    console.log(`String Value: ${taxValue.charAt(0)}`);
+}
--- tschap7/src/index.ts
+++ tschap7/src/index.ts
@@ -2,13 +2,14 @@ function calculateTax(amount:number, format: boolean): string|number{
     const calcAmount = amount * 1.2;
     return format ? `$${(amount*1.2).toFixed(2)}`:calcAmount;
 }
-let taxNumber = calculateTax(100, false) as number;
-let taxString = calculateTax(100, true)as string;
-let taxBoolean = calculateTax(100, false) as any as boolean;
+let taxValue = calculateTax(100, false);
 
-console.log(`Number value: ${taxNumber.toFixed(2)}`);
-console.log(`String value: ${taxString.charAt(0)}`);
-console.log(`Boolean Value: ${taxBoolean}`);
+if(typeof taxValue === "number"){
+    console.log(`Number Value: ${taxValue.toFixed(2)}`);
+}else if (typeof taxValue === "string"){
+    console.log(`String Value: ${taxValue.charAt(0)}`);
+}
+
 
 
 
