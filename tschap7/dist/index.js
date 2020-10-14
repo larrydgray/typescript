59425440b0f1535d3a605b6813e10f5c528d88fe
caverdude <javacaverdude@gmail.com>
2020-10-14 17:41:47 -0500

    Chapter7 Static types

--- tschap7/dist/index.js
+++ tschap7/dist/index.js
@@ -4,5 +4,7 @@ function calculateTax(amount, format) {
 }
 let taxNumber = calculateTax(100, false);
 let taxString = calculateTax(100, true);
+let taxBoolean = calculateTax(100, false);
 console.log(`Number value: ${taxNumber.toFixed(2)}`);
 console.log(`String value: ${taxString.charAt(0)}`);
+console.log(`Boolean Value: ${taxBoolean}`);
--- tschap7/src/index.ts
+++ tschap7/src/index.ts
@@ -4,9 +4,11 @@ function calculateTax(amount:number, format: boolean): string|number{
 }
 let taxNumber = calculateTax(100, false) as number;
 let taxString = calculateTax(100, true)as string;
+let taxBoolean = calculateTax(100, false) as boolean;
 
 console.log(`Number value: ${taxNumber.toFixed(2)}`);
 console.log(`String value: ${taxString.charAt(0)}`);
+console.log(`Boolean Value: ${taxBoolean}`);
 
 
 
