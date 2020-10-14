96a21bc38e08bcce2fe0872238c515dd72cf424a
caverdude <javacaverdude@gmail.com>
2020-10-14 17:25:33 -0500

    Chapter7 Static types

--- tschap7/src/index.ts
+++ tschap7/src/index.ts
@@ -5,7 +5,8 @@ function calculateTax(amount:number, format: boolean): string|number{
 let taxNumber = calculateTax(100, false);
 let taxString = calculateTax(100, true);
 
-
+console.log(`Number value: ${taxNumber.toFixed(2)}`);
+console.log(`String value: ${taxString.charAt(0)}`);
 
 
 
