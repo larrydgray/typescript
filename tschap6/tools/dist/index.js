bff985d95458670d358a9d367bb88eca99e60e78
caverdude <javacaverdude@gmail.com>
2020-10-13 19:59:02 -0500

    chp6 debugging, linting, unit testing

---
+++ tschap6/tools/dist/index.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var calc_1 = require("./calc");
+var printMessage = function (msg) {
+    return console.log("Message: " + msg);
+};
+var message = ("Hello, TypeScript");
+printMessage(message);
+var total = calc_1.sum(100, 200, 300);
+console.log("Total: " + total);
+//
+//# sourceMappingURL=index.js.map
