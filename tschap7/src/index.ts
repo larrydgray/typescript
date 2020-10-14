9a0cded9bef825cae3c6b32871b4046fff775709
caverdude <javacaverdude@gmail.com>
2020-10-14 15:46:35 -0500

    Chapter7 Static types

--- tschap7/src/index.ts
+++ tschap7/src/index.ts
@@ -1,10 +1,11 @@
-function calculateTax(amount:number){
+function calculateTax(amount:any):any{
     return (amount*1.2).toFixed(2);
 }
 let price: number = 100;
-let taxAmount: number = calculateTax(price);
-let halfShare: = taxAmount /2;
+let taxAmount = calculateTax(price);
+let halfShare = taxAmount / 2;
 
+console.log(`Price: ${price}`);
 console.log(`Full amount in tax: ${taxAmount}`);
 console.log(`Half Share: ${halfShare}`);
 
