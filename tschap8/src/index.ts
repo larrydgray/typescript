e72e9ea11e6af5a3fd1059cabf44d3893059f024
caverdude <javacaverdude@gmail.com>
2020-10-15 00:29:18 -0500

    chp6 debugging, linting, unit testing

--- tschap8/src/index.ts
+++ tschap8/src/index.ts
@@ -9,3 +9,6 @@ taxValue = calculateTax(100);
 console.log(`1 arg: ${taxValue}`);
 taxValue = calculateTax(100, 10, 20);
 console.log(`3 args: ${taxValue}`);
+taxValue = calculateTax(100,10,20,1,30,7);
+console.log(`6 args: ${taxValue}`);
+
