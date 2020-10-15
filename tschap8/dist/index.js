bb985e54e574c16021e8675e2805ba280e4fbbf6
caverdude <javacaverdude@gmail.com>
2020-10-15 00:17:30 -0500

    chp6 debugging, linting, unit testing

--- tschap8/dist/index.js
+++ tschap8/dist/index.js
@@ -1,9 +1,9 @@
-function calculateTax(amount, discount) {
+function calculateTax(amount, discount = 0) {
     return (amount * 1.2) - discount;
 }
 let taxValue = calculateTax(100, 0);
 console.log(`2 args: ${taxValue}`);
 taxValue = calculateTax(100);
 console.log(`1 arg: ${taxValue}`);
-taxValue = calculateTax(100, 10, 20);
-console.log(`3 args: ${taxValue}`);
+//taxValue = calculateTax(100, 10, 20);
+//console.log(`3 args: ${taxValue}`);
