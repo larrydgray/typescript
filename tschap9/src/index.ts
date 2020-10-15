e6a96e3d6511d8396f1e17101d77e9dc3137b71e
caverdude <javacaverdude@gmail.com>
2020-10-15 14:51:15 -0500

    chp 9 arrays, tuples and enums

--- tschap9/src/index.ts
+++ tschap9/src/index.ts
@@ -6,7 +6,7 @@ function writePrice(product: string, price:number):void {
     console.log(`Price for ${product}: $${price.toFixed(2)}`);
 }
 
-let prices: number[] = [100,75,42];
+let prices: Array<number>= [100,75,42];
 let names: string[] = ["Hat", "Gloves", "Umbrella"];
 
 prices.forEach((price: number, index: number)=>{
