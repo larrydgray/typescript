557e6f7b52ee2f97769733c87deeb94edb4651ed
caverdude <javacaverdude@gmail.com>
2020-10-15 01:30:51 -0500

    chp6 debugging, linting, unit testing

--- tschap8/dist/index.js
+++ tschap8/dist/index.js
@@ -1,11 +1,6 @@
 function calculateTax(amount, discount = 0, ...extraFees) {
-    if (amount != null) {
-        return (amount * 1.2) - discount
-            + extraFees.reduce((total, val) => total + val, 0);
-    }
-    else {
-        return undefined;
-    }
+    return (amount * 1.2) - discount
+        + extraFees.reduce((total, val) => total + val, 0);
 }
-let taxValue = calculateTax(null, 0);
+let taxValue = calculateTax(100, 0);
 console.log(`Tax value: ${taxValue}`);
--- tschap8/src/index.ts
+++ tschap8/src/index.ts
@@ -1,13 +1,11 @@
 
-function calculateTax(amount: number|null,discount=0, ...extraFees:number[]){
-    if(amount!=null) {
+function calculateTax(amount: number,discount=0, ...extraFees:number[]):number{
+
         return (amount * 1.2) - discount
             + extraFees.reduce((total, val) => total + val, 0);
-    }else{
-        return undefined;
-    }
+
 }
-let taxValue=calculateTax(null,0);
+let taxValue=calculateTax(100,0);
 console.log(`Tax value: ${taxValue}`);
 
 
