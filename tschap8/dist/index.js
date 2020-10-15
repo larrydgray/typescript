8dffb4b8a9cbb40fc2323971e1800eb1dec78923
caverdude <javacaverdude@gmail.com>
2020-10-15 01:25:36 -0500

    chp6 debugging, linting, unit testing

--- tschap8/dist/index.js
+++ tschap8/dist/index.js
@@ -3,6 +3,9 @@ function calculateTax(amount, discount = 0, ...extraFees) {
         return (amount * 1.2) - discount
             + extraFees.reduce((total, val) => total + val, 0);
     }
+    else {
+        return undefined;
+    }
 }
 let taxValue = calculateTax(null, 0);
 console.log(`Tax value: ${taxValue}`);
