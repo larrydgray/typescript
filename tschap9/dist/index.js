d93ca3723ede1b87d62b2434902fa95894aac516
caverdude <javacaverdude@gmail.com>
2020-10-15 15:19:29 -0500

    chp 9 arrays, tuples and enums

--- tschap9/dist/index.js
+++ tschap9/dist/index.js
@@ -4,8 +4,8 @@ function calculateTax(amount) {
 function writePrice(product, price) {
     console.log(`Price for ${product}: $${price.toFixed(2)}`);
 }
-let prices = [100, 75, 42];
-let names = ["Hat", "Gloves", "Umbrella"];
+let prices = [100, 75, 42, "20"];
+let names = ["Hat", "Gloves", "Umbrella", "Sunglasses"];
 prices.forEach((price, index) => {
     writePrice(names[index], calculateTax(price));
 });
