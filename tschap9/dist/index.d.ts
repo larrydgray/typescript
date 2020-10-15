5987bd95c9e87c2546a47e372767bfeb92c56e5e
caverdude <javacaverdude@gmail.com>
2020-10-15 17:26:52 -0500

    chp 9 arrays, tuples and enums

--- tschap9/dist/index.d.ts
+++ tschap9/dist/index.d.ts
@@ -1,9 +1,13 @@
 declare function calculateTax(amount: number): number;
 declare function writePrice(product: string, price: number): void;
+declare enum OtherEnum {
+    First = 10,
+    Two = 20
+}
 declare enum Product {
-    Hat = 0,
+    Hat = 11,
     Gloves = 20,
-    Umbrella = 21
+    Umbrella = 31
 }
 declare let productValue: Product;
 declare let productName: string;
--- tschap9/dist/index.js
+++ tschap9/dist/index.js
@@ -4,12 +4,17 @@ function calculateTax(amount) {
 function writePrice(product, price) {
     console.log(`Price for ${product}: $${price.toFixed(2)}`);
 }
+var OtherEnum;
+(function (OtherEnum) {
+    OtherEnum[OtherEnum["First"] = 10] = "First";
+    OtherEnum[OtherEnum["Two"] = 20] = "Two";
+})(OtherEnum || (OtherEnum = {}));
 var Product;
 (function (Product) {
-    Product[Product["Hat"] = 0] = "Hat";
+    Product[Product["Hat"] = 11] = "Hat";
     Product[Product["Gloves"] = 20] = "Gloves";
-    Product[Product["Umbrella"] = 21] = "Umbrella";
+    Product[Product["Umbrella"] = 31] = "Umbrella";
 })(Product || (Product = {}));
-let productValue = 2;
+let productValue = 11;
 let productName = Product[productValue];
 console.log(`Value: ${productValue}, Name: ${productName}`);
