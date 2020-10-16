610f1a4399d165d7f21fe628d8030f841130d5c8
caverdude <javacaverdude@gmail.com>
2020-10-15 23:06:53 -0500

    Chapter 10 Objects

--- tschap10/dist/index.js
+++ tschap10/dist/index.js
@@ -1,5 +1,11 @@
+var Feature;
+(function (Feature) {
+    Feature[Feature["Waterproof"] = 0] = "Waterproof";
+    Feature[Feature["Insulated"] = 1] = "Insulated";
+})(Feature || (Feature = {}));
 let hat = { name: "Hat", price: 100 };
 let gloves = { name: "Gloves", price: 75 };
-let umbrella = { name: "Umbrella", price: 30, waterproof: true };
+let umbrella = { name: "Umbrella", price: 30, hasFeature: (feature) => feature === Feature.Waterproof };
 let products = [hat, gloves, umbrella];
-products.forEach(prod => console.log(`${prod.name}: ${prod.price} Waterproof: ${prod.waterproof}`));
+products.forEach(prod => console.log(`${prod.name}: ${prod.price} 
+         Waterproof: ${prod.hasFeature(Feature.Waterproof)}`));
--- tschap10/src/index.ts
+++ tschap10/src/index.ts
@@ -1,9 +1,15 @@
+enum Feature { Waterproof, Insulated}
+
 let hat = {name: "Hat", price: 100};
 let gloves = {name: "Gloves", price: 75};
-let umbrella = {name: "Umbrella", price:30, waterproof:true};
+let umbrella = {name: "Umbrella", price:30, hasFeature:(feature)=>
+        feature === Feature.Waterproof};
 
-let products:{ name: string, price?:number, waterproof?: boolean}[] = [hat, gloves, umbrella];
+let products: {name: string, price?:number, hasFeature?(Feature):boolean}[]
+        =[hat,gloves,umbrella];
 
-products.forEach(prod => console.log(`${prod.name}: ${prod.price} Waterproof: ${prod.waterproof}`));
+
+products.forEach(prod => console.log(`${prod.name}: ${prod.price} 
+         Waterproof: ${prod.hasFeature(Feature.Waterproof)}`));
 
 
