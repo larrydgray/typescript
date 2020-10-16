b8655fe65da68fdba28ad3e52275a6b63fbc701d
caverdude <javacaverdude@gmail.com>
2020-10-15 20:27:25 -0500

    literal values

--- tschap9/dist/index.js
+++ tschap9/dist/index.js
@@ -3,3 +3,26 @@ function calculatePrice(quantity, price) {
 }
 let total = calculatePrice(2, 19.99);
 console.log(`Price: ${total}`);
+function getRandomValue() {
+    return Math.floor(Math.random() * 4) + 1;
+}
+var City;
+(function (City) {
+    City["London"] = "LON";
+    City["Paris"] = "PAR";
+    City["Chicago"] = "CHI";
+})(City || (City = {}));
+function getMixedValue() {
+    switch (getRandomValue()) {
+        case 1:
+            return 1;
+        case 2:
+            return "Hello";
+        case 3:
+            return true;
+        case 4:
+            return City.London;
+    }
+}
+////////////
+console.log(`Value: ${getMixedValue()}`);
