910462172f7bbefcba5512ead038064c7850dd99
caverdude <javacaverdude@gmail.com>
2020-10-16 15:49:08 -0500

    Chapter 10 Objects

--- tschap10/dist/index.js
+++ tschap10/dist/index.js
@@ -1,9 +1,9 @@
 let typeTest = {}.contact;
 let person1 = {
     id: "bsmith", name: "Bob Smith", city: "London",
-    company: "Acme Co", dept: "Sales", contact: "Alice"
+    company: "Acme Co", dept: "Sales", contact: { name: "Alice", phone: 6512346543 }
 };
 let person2 = {
     id: "dpeters", name: "Dora Peters", city: "New York",
-    company: "Acme Co", dept: "Development", contact: 6512346543
+    company: "Acme Co", dept: "Development", contact: { name: "Alice", phone: 6512346543 }
 };
--- tschap10/src/index.ts
+++ tschap10/src/index.ts
@@ -4,14 +4,14 @@ type Person = {
   id: string,
   name: string,
   city: string,
-  contact: number
+  contact: {phone:number}
 };
 
 type Employee= {
   id:string,
   company: string,
   dept: string,
-  contact: string
+  contact: {name:string}
 };
 
 type EmployedPerson = Person & Employee;
@@ -20,10 +20,10 @@ let typeTest = ({} as EmployedPerson).contact;
 
 let person1: EmployedPerson = {
     id: "bsmith", name: "Bob Smith", city: "London",
-    company: "Acme Co", dept: "Sales", contact: "Alice"};
+    company: "Acme Co", dept: "Sales", contact: {name: "Alice", phone: 6512346543}};
 let person2: EmployedPerson = {
     id: "dpeters", name: "Dora Peters", city: "New York",
-    company: "Acme Co", dept: "Development", contact: 6512346543};
+    company: "Acme Co", dept: "Development", contact: {name: "Alice", phone: 6512346543}};
 
 
 
