bf7aecaa13e08818391ea09a456e673185883fc7
caverdude <javacaverdude@gmail.com>
2020-10-15 22:09:54 -0500

    Chapter 10 Objects

--- tschap10/dist/index.js
+++ tschap10/dist/index.js
@@ -1,5 +1,5 @@
 let hat = { name: "Hat", price: 100 };
 let gloves = { name: "Gloves", price: 75 };
-//let umbrella = {name: "Umbrella"};
-let products = [hat, gloves];
+let umbrella = { name: "Umbrella" };
+let products = [hat, gloves, umbrella];
 products.forEach(prod => console.log(`${prod.name}: ${prod.price}`));
