4dd6a5d598234e2e18866cd75c0c791c6582988a
caverdude <javacaverdude@gmail.com>
2020-10-15 19:48:35 -0500

    literal values

--- tschap9/dist/index.d.ts
+++ tschap9/dist/index.d.ts
@@ -1,1 +1,2 @@
 declare let restrictedValue: 1 | 2 | 3;
+declare let secondValue: 1 | 10 | 100;
--- tschap9/dist/index.js
+++ tschap9/dist/index.js
@@ -1,2 +1,6 @@
-let restrictedValue = 100;
+let restrictedValue = 1;
+let secondValue = 1;
+restrictedValue = secondValue;
+secondValue = 100;
+restrictedValue = secondValue;
 console.log(`Value: ${restrictedValue}`);
