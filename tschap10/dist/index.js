71b4d8346ea06203691bfdcd15bf035cdb320f2b
caverdude <javacaverdude@gmail.com>
2020-10-16 17:15:16 -0500

    Chapter 10 Objects

--- tschap10/dist/index.js
+++ tschap10/dist/index.js
@@ -1,9 +1,12 @@
-let typeTest = {}.contact;
-let person1 = {
+let person = {
     id: "bsmith", name: "Bob Smith", city: "London",
-    company: "Acme Co", dept: "Sales", contact: { name: "Alice", phone: 6512346543 }
+    company: "Acme Co", dept: "Sales",
+    getContact(field) {
+        return typeof field === "string" ? "Alice" : 6512346543;
+    }
 };
-let person2 = {
-    id: "dpeters", name: "Dora Peters", city: "New York",
-    company: "Acme Co", dept: "Development", contact: { name: "Alice", phone: 6512346543 }
-};
+let typeTest = person.getContact;
+let stringParamTypeTest = person.getContact("Alice");
+let numberParamTypeTest = person.getContact(123);
+console.log(`Contact: ${person.getContact("Alice")}`);
+console.log(`Contact: ${person.getContact(1234)}`);
