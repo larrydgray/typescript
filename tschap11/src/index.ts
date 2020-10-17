420a7197b0b3b1edc5398344dea6c769e16e326a
caverdude <javacaverdude@gmail.com>
2020-10-16 20:19:21 -0500

    Chapter 10 Objects

--- tschap11/src/index.ts
+++ tschap11/src/index.ts
@@ -5,10 +5,10 @@ type Person = {
 };
 
 class Employee {
-    id: string;
-    name: string;
-    dept: string;
-    city: string;
+    public id: string;
+    public name: string;
+    private dept: string;
+    public city: string;
     constructor(id: string, name: string, dept: string, city:string) {
         this.id = id;
         this.name = name;
@@ -22,6 +22,10 @@ class Employee {
 
 let salesEmployee = new Employee("fvega", "Fidel Vego", "Sales", "Paris");
 
+console.log(`Dept value: ${salesEmployee.dept}`);
+
+
+/*
 let data: (Person | Employee)[] =
     [{id: "bsmith", name: "Bob Smith", city: "London"},
     {id: "ajones", name: "Alice Jones", city: "Paris"},
@@ -34,4 +38,4 @@ data.forEach(item=>{
     }else {
         console.log(`${item.id} ${item.name}, ${item.city}`);
     }
-});
+});*/
