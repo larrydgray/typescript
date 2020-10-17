49288c9bec23d1dceef72f3bd415c1a7a74b871e
caverdude <javacaverdude@gmail.com>
2020-10-16 23:20:01 -0500

    Chapter 10 Objects

--- tschap11/dist/index.js
+++ tschap11/dist/index.js
@@ -49,36 +49,8 @@ class Supplier extends Person {
 }
 let data = [new Employee("fvega", "Fidel Vega", "Sales", "Paris"),
     new Customer("ajones", "Alice Jones", "London", 500)];
+;
+data.push(new Supplier("dpeters", "Dora Peters", "New York", "Acme"));
 data.forEach(item => {
     console.log(item.getDetails());
 });
-data.push(new Supplier("dpeters", "Dora Peters", "New York", "Acme"));
-data.forEach(item => {
-    console.log(`Person: ${item.name}, ${item.city}`);
-    if (item instanceof Employee) {
-        item.writeDept();
-    }
-    else if (item instanceof Customer) {
-        console.log(`Customer ${item.name} has  ${item.creditLimit} limit`);
-    }
-    else if (item instanceof Supplier) {
-        console.log(`Supplier ${item.name} works for ${item.companyName}`);
-    }
-});
-//let salesEmployee = new Employee("fvega", "Fidel Vego", "Sales", "Paris");
-//salesEmployee.writeDept();
-//salesEmployee.id="fidel";
-/*
-let data: (Person | Employee)[] =
-    [{id: "bsmith", name: "Bob Smith", city: "London"},
-    {id: "ajones", name: "Alice Jones", city: "Paris"},
-    {id: "dpeters", name:"Dora Peters", city: "New York"},
-    salesEmployee];
-
-data.forEach(item=>{
-    if(item instanceof Employee){
-        item.writeDept();
-    }else {
-        console.log(`${item.id} ${item.name}, ${item.city}`);
-    }
-});*/ 
--- tschap11/src/index.ts
+++ tschap11/src/index.ts
@@ -48,40 +48,9 @@ class Supplier extends  Person{
 let data: Person[] = [new Employee("fvega", "Fidel Vega", "Sales","Paris"),
     new Customer("ajones", "Alice Jones", "London",500)];
 
-data.forEach(item => {
-    console.log(item.getDetails());
-});
+;
 
 data.push( new Supplier("dpeters", "Dora Peters", "New York", "Acme"));
-
-data.forEach(item=>{
-    console.log(`Person: ${item.name}, ${item.city}`);
-    if(item instanceof Employee){
-        item.writeDept();
-    }else if(item instanceof Customer){
-        console.log(`Customer ${item.name} has  ${item.creditLimit} limit`);
-    }else if(item instanceof  Supplier){
-        console.log(`Supplier ${item.name} works for ${item.companyName}`);
-    }
-
-});
-
-//let salesEmployee = new Employee("fvega", "Fidel Vego", "Sales", "Paris");
-
-//salesEmployee.writeDept();
-//salesEmployee.id="fidel";
-
-/*
-let data: (Person | Employee)[] =
-    [{id: "bsmith", name: "Bob Smith", city: "London"},
-    {id: "ajones", name: "Alice Jones", city: "Paris"},
-    {id: "dpeters", name:"Dora Peters", city: "New York"},
-    salesEmployee];
-
-data.forEach(item=>{
-    if(item instanceof Employee){
-        item.writeDept();
-    }else {
-        console.log(`${item.id} ${item.name}, ${item.city}`);
-    }
-});*/
+data.forEach(item => {
+    console.log(item.getDetails());
+})
