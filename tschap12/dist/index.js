8994db34a04d2bd37846c8c2119fc53ab52ff6c3
caverdude <javacaverdude@gmail.com>
2020-10-18 18:04:11 -0500

    Chapter 12 generics

--- tschap12/dist/index.js
+++ tschap12/dist/index.js
@@ -4,4 +4,22 @@ const dataTypes_1 = require("./dataTypes");
 let people = [new dataTypes_1.Person("Bob Smith", "London"),
     new dataTypes_1.Person("Dora Peters", "New York")];
 let products = [new dataTypes_1.Product("Running Shoes", 100), new dataTypes_1.Product("Hat", 25)];
-[...people, ...products].forEach(item => console.log(`Item: ${item.name}`));
+class PeopleCollection {
+    constructor(initialItems) {
+        this.items = [];
+        this.items.push(...initialItems);
+    }
+    add(newItem) {
+        this.items.push(newItem);
+    }
+    getNames() {
+        return this.items.map(item => item.name);
+    }
+    getItem(index) {
+        return this.items[index];
+    }
+}
+let peopleData = new PeopleCollection(people);
+console.log(`Names: ${peopleData.getNames().join(", ")}`);
+let firstPerson = peopleData.getItem(0);
+console.log(`First Person: ${firstPerson.name}, ${firstPerson.city}`);
