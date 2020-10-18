eabc2c602d235036dc5f01fe87b6902c8e88a9fb
caverdude <javacaverdude@gmail.com>
2020-10-17 22:04:03 -0500

    Chapter 10 Objects

--- tschap11/dist/index.js
+++ tschap11/dist/index.js
@@ -1,12 +1,3 @@
-class Employee {
-    constructor(name, company) {
-        this.name = name;
-        this.company = company;
-    }
-    getDetails() {
-        return `${this.name} works for ${this.company}`;
-    }
-}
 class SportsProduct {
     constructor(name, category, price) {
         this.name = name;
@@ -14,14 +5,11 @@ class SportsProduct {
         this.price = price;
     }
 }
-let data = [new Employee("Bob Smith", "Acme"),
-    new SportsProduct("Running Shoes", "Running", 90.50),
-    new Employee("Dora Peters", "BigCo")];
-data.forEach(item => {
-    if ("getDetails" in item) {
-        console.log(`Person: ${item.getDetails()}`);
+class ProductGroup {
+    constructor(...initialProducts) {
+        initialProducts.forEach(p => this[p[0]] = p[1]);
     }
-    else {
-        console.log(`Product: ${item.name}, ${item.price}`);
-    }
-});
+}
+let group = new ProductGroup(["shoes", new SportsProduct("Shoes", "Running", 90.50)]);
+group.hat = new SportsProduct("Hat", "Skiing", 20);
+Object.keys(group).forEach(k => console.log(`Property Name: ${k}`));
--- tschap11/src/index.ts
+++ tschap11/src/index.ts
@@ -1,40 +1,27 @@
-interface Person{
-    name:string;
-    getDetails(): string;
 
-}
 
 interface Product{
     name: string;
     price: number;
 }
 
-class Employee implements Person{
-    constructor(public name: string, public company: string) {
 
-    }
-
-    getDetails(): string {
-        return `${this.name} works for ${this.company}`;
-    }
-
-}
 
 class SportsProduct implements Product {
     constructor(public name: string, public category: string,
                 public price: number) {
 
     }
+
+}
+class ProductGroup {
+    constructor(...initialProducts: [string, Product][]) {
+        initialProducts.forEach(p => this[p[0]]=p[1]);
+    }
+    [propertyName: string]: Product;
 }
 
-let data: (Person | Product)[] = [new Employee("Bob Smith", "Acme"),
-    new SportsProduct("Running Shoes", "Running",90.50),
-    new Employee("Dora Peters", "BigCo")];
-
-data.forEach(item=>{
-    if("getDetails" in item){
-        console.log(`Person: ${item.getDetails()}`);
-    } else {
-        console.log(`Product: ${item.name}, ${item.price}`);
-    }
-});
+let group
+= new ProductGroup(["shoes", new SportsProduct("Shoes", "Running", 90.50)]);
+group.hat = new SportsProduct("Hat", "Skiing", 20);
+Object.keys(group).forEach(k => console.log(`Property Name: ${k}`));
