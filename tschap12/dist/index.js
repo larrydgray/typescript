f90b0b96a5134228a0e320e73ca426834122a21f
caverdude <javacaverdude@gmail.com>
2020-10-18 23:38:04 -0500

    Chapter 12 generics

--- tschap12/dist/index.js
+++ tschap12/dist/index.js
@@ -1,2 +1,20 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+const dataTypes_1 = require("./dataTypes");
+class ArrayCollection {
+    constructor() {
+        this.items = [];
+    }
+    add(...newItems) {
+        this.items.push(...newItems);
+    }
+    get(name) {
+        return this.items.find(item => item.name === name);
+    }
+    get count() {
+        return this.items.length;
+    }
+}
+let peopleCollection = new ArrayCollection();
+peopleCollection.add(new dataTypes_1.Person("Bob Smith", "London"), new dataTypes_1.Person("Dora Peters", "New York"));
+console.log(`Collection size: ${peopleCollection.count}`);
