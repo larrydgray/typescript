d3842bb1c3926d257874eb6e74fa94d24527a549
caverdude <javacaverdude@gmail.com>
2020-10-19 18:07:43 -0500

    Chapter 13 advanced generics

--- tschap13/dist/index.js
+++ tschap13/dist/index.js
@@ -1,5 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-let firstVal = "String Value";
-let secondVal = 100;
-let mistmatchCheck = "String Value";
+const dataTypes_1 = require("./dataTypes");
+let firstVal = new dataTypes_1.City("London", 8136000);
+let secondVal = new dataTypes_1.Person("Bob", "London");
+let thirdVal = new dataTypes_1.Product("Kayak", 275);
