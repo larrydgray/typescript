d4d66cee5a4fe8f5e907bcec2ec3025ac0cbf8d0
caverdude <javacaverdude@gmail.com>
2020-10-19 17:39:50 -0500

    Chapter 13 advanced generics

--- tschap13/src/index.ts
+++ tschap13/src/index.ts
@@ -1,11 +1,13 @@
 import { City, Person, Product, Employee} from "./dataTypes";
 
-type SelectProperties<T, K extends keyof T>={
-        [P in K]: T[P]
+type CustomMapped<T, K extends keyof T>={
+        readonly[P in K]?: T[P]
 };
 
-let p1: SelectProperties<Product, "name">={name: "Kayak"};
-let p2: Pick<Product, "name"| "price"> = {name: "Lifejacket", price: 48.95};
+type BuiltInMapped<T, K extends keyof T> = Readonly<Partial<Pick<T, K>>>;
+
+let p1: CustomMapped<Product, "name">={name: "Kayak"};
+let p2: BuiltInMapped<Product, "name"| "price"> = {name: "Lifejacket", price: 48.95};
 
 console.log(`Custom mapped type: ${p1.name}`);
 console.log(`Mapped type: ${p2.name}, ${p2.price}`);
