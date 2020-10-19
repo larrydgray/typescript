a6eb966daab21a5ccbbe0b595cc455e87d22833c
caverdude <javacaverdude@gmail.com>
2020-10-19 15:36:45 -0500

    Chapter 13 advanced generics

--- tschap13/dist/index.js
+++ tschap13/dist/index.js
@@ -4,11 +4,11 @@ const dataTypes_1 = require("./dataTypes");
 let myVar = "name";
 myVar = "price";
 function getValue(item, keyname) {
-    console.log(`Value: ${item[keyname]}`);
+    return item[keyname];
 }
 let p = new dataTypes_1.Product("Running Shoes", 100);
-getValue(p, "name");
-getValue(p, "price");
+console.log(getValue(p, "name"));
+console.log(getValue(p, "price"));
 let e = new dataTypes_1.Employee("Bob Smith", "Sales");
-getValue(e, "name");
-getValue(e, "role");
+console.log(getValue(e, "name"));
+console.log(getValue(e, "role"));
