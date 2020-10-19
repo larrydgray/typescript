6a4c0467d4aeb039158fe98fe3e6660817e0b4fb
caverdude <javacaverdude@gmail.com>
2020-10-19 16:11:57 -0500

    Chapter 13 advanced generics

--- tschap13/dist/index.js
+++ tschap13/dist/index.js
@@ -1,14 +1,27 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const dataTypes_1 = require("./dataTypes");
-let myVar = "name";
-myVar = "price";
-function getValue(item, keyname) {
-    return item[keyname];
+let products = [new dataTypes_1.Product("Running Shoes", 100), new dataTypes_1.Product("Hat", 25)];
+class Collection {
+    constructor(initialItems = [], propertyName) {
+        this.propertyName = propertyName;
+        this.items = new Map();
+        this.add(...initialItems);
+    }
+    add(...newItems) {
+        newItems.forEach(newItem => this.items.set(newItem[this.propertyName], newItem));
+    }
+    get(key) {
+        return this.items.get(key);
+    }
+    get count() {
+        return this.items.size;
+    }
+    [Symbol.iterator]() {
+        return this.items.values();
+    }
 }
-let p = new dataTypes_1.Product("Running Shoes", 100);
-console.log(getValue(p, "name"));
-console.log(getValue(p, "price"));
-let e = new dataTypes_1.Employee("Bob Smith", "Sales");
-console.log(getValue(e, "name"));
-console.log(getValue(e, "role"));
+let productCollection = new Collection(products, "name");
+console.log(`There are ${productCollection.count} products`);
+let itemByKey = productCollection.get("Hat");
+console.log(`Item: ${itemByKey.name}, ${itemByKey.price}`);
