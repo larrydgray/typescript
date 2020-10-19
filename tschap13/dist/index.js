4991db93df71a87b5fecf34f7433dbfba492dfb1
caverdude <javacaverdude@gmail.com>
2020-10-19 13:57:31 -0500

    Chapter 13 advanced generics

--- tschap13/dist/index.js
+++ tschap13/dist/index.js
@@ -3,17 +3,18 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const dataTypes_1 = require("./dataTypes");
 let products = [new dataTypes_1.Product("Running Shoes", 100), new dataTypes_1.Product("Hat", 25)];
 class Collection {
-    constructor(items = []) {
-        this.items = items;
+    constructor(initialItems = []) {
+        this.initialItems = initialItems;
+        this.items = new Set(initialItems);
     }
     add(...newItems) {
-        this.items.push(...newItems);
+        newItems.forEach(newItem => this.items.add(newItem));
     }
     get(name) {
-        return this.items.find(item => item.name === name);
+        return [...this.items.values()].find(item => item.name === name);
     }
     get count() {
-        return this.items.length;
+        return this.items.size;
     }
 }
 let productCollection = new Collection(products);
--- tschap13/src/index.ts
+++ tschap13/src/index.ts
@@ -5,18 +5,20 @@ let products = [new Product("Running Shoes",100), new Product("Hat", 25)];
 type shapeType = {name:string};
 
 class Collection<T extends shapeType>{
-        constructor(private items: T[]=[]) {
-
+        private items: Set<T>;
+        constructor(private initialItems: T[]=[]) {
+                this.items = new Set<T>(initialItems);
         }
+
 
         add(...newItems:T[]): void {
-                this.items.push(...newItems);
+                newItems.forEach(newItem => this.items.add(newItem));
         }
         get(name:string): T{
-                return this.items.find(item=> item.name === name);
+                return [...this.items.values()].find(item => item.name === name);
         }
         get count(): number {
-                return this.items.length;
+                return this.items.size;
         }
 }
 let productCollection: Collection<Product> = new Collection(products);
