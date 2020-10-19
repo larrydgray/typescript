cc9936f07ab6c80460cddc190a0d51cdc0a4f4b5
caverdude <javacaverdude@gmail.com>
2020-10-18 23:27:28 -0500

    Chapter 12 generics

--- tschap12/src/index.ts
+++ tschap12/src/index.ts
@@ -1,37 +1,7 @@
-import {City, Person, Product, Employee} from "./dataTypes";
-
-let people = [new Person("Bob Smith","London"),
-        new Person("Dora Peters", "New York")];
-let products = [new Product("Running Shoes", 100), new Product("Hat", 25)];
-let cities = [new City("London", 8136000), new City("Paris", 2141000)];
-let employees = [new Employee("Bob Smith", "Sales"),
-        new Employee("Alice Jones", "Sales")];
-
-//type dataType = Person | Product;
-
-class DataCollection<T> {
-
-        protected items: T[] = [];
-
-        constructor(initialItems: T[]) {
-                this.items.push(...initialItems);
-        }
-        filter<V extends T>(predicate:(target)=>target is V): V[] {
-                return this.items.filter(item => predicate(item)) as V[];
-        };
-        static reverse<ArrayType>(items: any[]): ArrayType[]{
-                return items.reverse();
-        }
+import { City, Person, Product, Employee} from "./dataTypes";
+type shapeType = {name:string};
+interface Collection<T extends shapeType> {
+        add(...newItems: T[]): void;
+        get(name:string):T;
+        count:number;
 }
-
-let mixedData = new DataCollection<Person | Product>([...people, ...products]);
-function isProduct(target): target is Product {
-        return target instanceof Product;
-}
-let filteredProducts = mixedData.filter<Product>(isProduct);
-filteredProducts.forEach(p => console.log(`Product ${p.name}, ${p.price}`));
-
-let reversedCities: City[] = DataCollection.reverse<City>(cities);
-reversedCities.forEach(c => console.log(`City: ${c.name}, ${c.population}`));
-
-
