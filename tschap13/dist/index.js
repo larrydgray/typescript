ebc710d8cd3d286d417ce526c9686e0629b34cd5
caverdude <javacaverdude@gmail.com>
2020-10-19 16:30:26 -0500

    Chapter 13 advanced generics

--- tschap13/dist/index.js
+++ tschap13/dist/index.js
@@ -1,27 +1,4 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const dataTypes_1 = require("./dataTypes");
-let products = [new dataTypes_1.Product("Running Shoes", 100), new dataTypes_1.Product("Hat", 25)];
-class Collection {
-    constructor(initialItems = [], propertyName) {
-        this.propertyName = propertyName;
-        this.items = new Map();
-        this.add(...initialItems);
-    }
-    add(...newItems) {
-        newItems.forEach(newItem => this.items.set(newItem[this.propertyName], newItem));
-    }
-    get(key) {
-        return this.items.get(key);
-    }
-    get count() {
-        return this.items.size;
-    }
-    [Symbol.iterator]() {
-        return this.items.values();
-    }
-}
-let productCollection = new Collection(products, "price");
-console.log(`There are ${productCollection.count} products`);
-let itemByKey = productCollection.get(100);
-console.log(`Item: ${itemByKey.name}, ${itemByKey.price}`);
+let p = { name: "Kayak", price: 275 };
+console.log(`Mapped type: ${p.name}, ${p.price}`);
