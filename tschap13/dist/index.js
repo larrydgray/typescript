916eabced8b5ffa32ad3923da899a06a97583b1f
caverdude <javacaverdude@gmail.com>
2020-10-19 15:18:24 -0500

    Chapter 13 advanced generics

--- tschap13/dist/index.js
+++ tschap13/dist/index.js
@@ -1,29 +1,47 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const dataTypes_1 = require("./dataTypes");
-let products = [new dataTypes_1.Product("Running Shoes", 100), new dataTypes_1.Product("Hat", 25)];
-class Collection {
-    constructor(initialItems = []) {
-        this.initialItems = initialItems;
-        this.items = new Map();
-        this.add(...initialItems);
-    }
-    add(...newItems) {
-        newItems.forEach(newItem => this.items.set(newItem.name, newItem));
-    }
-    get(name) {
-        return this.items.get(name);
-    }
-    get count() {
-        return this.items.size;
-    }
-    [Symbol.iterator]() {
-        return this.items.values();
-    }
+let myVar = "name";
+myVar = "price";
+function getValue(item, keyname) {
+    console.log(`Value: ${item[keyname]}`);
 }
-let productCollection = new Collection(products);
+let p = new dataTypes_1.Product("Running Shoes", 100);
+getValue(p, "name");
+getValue(p, "price");
+let e = new dataTypes_1.Employee("Bob Smith", "Sales");
+getValue(e, "name");
+getValue(e, "role");
+/*
+let products = [new Product("Running Shoes",100), new Product("Hat", 25)];
+
+type shapeType = {name:string};
+
+class Collection<T extends shapeType> implements Iterable<T>{
+        private items: Map<string, T>;
+        constructor(private initialItems: T[]=[]) {
+                this.items = new Map<string,T>();
+                this.add(...initialItems);
+        }
+
+        add(...newItems:T[]): void {
+                newItems.forEach(newItem => this.items.set(newItem.name, newItem));
+        }
+        get(name:string): T{
+                return this.items.get(name);
+        }
+        get count(): number {
+                return this.items.size;
+        }
+        [Symbol.iterator]():Iterator<T>{
+                return this.items.values();
+        }
+}
+let productCollection: Collection<Product> = new Collection(products);
 console.log(`There are ${productCollection.count} products`);
-[...productCollection].forEach(p => console.log(`Product ${p.name}, ${p.price}`));
+
+[...productCollection].forEach(p=> console.log(`Product ${p.name}, ${p.price}`));
+
 /*
 let iterator: Iterator<Product>= productCollection.values();
 let result: IteratorResult<Product> = iterator.next();
--- tschap13/src/index.ts
+++ tschap13/src/index.ts
@@ -2,8 +2,20 @@ import { City, Person, Product, Employee} from "./dataTypes";
 
 let myVar: keyof Product= "name";
 myVar = "price";
-myVar = "someOtherName";
 
+function getValue<T, K extends keyof T> (item: T, keyname: K){
+        console.log(`Value: ${item[keyname]}`);
+}
+
+let p = new Product("Running Shoes", 100);
+getValue(p, "name");
+getValue(p, "price");
+
+let e = new Employee("Bob Smith", "Sales");
+getValue(e, "name");
+getValue(e, "role");
+
+/*
 let products = [new Product("Running Shoes",100), new Product("Hat", 25)];
 
 type shapeType = {name:string};
