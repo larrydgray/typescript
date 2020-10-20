8b8b0fbd4c438d3491461e59f443f4041c4ae98f
caverdude <javacaverdude@gmail.com>
2020-10-20 18:15:15 -0500

    Chapter 13 advanced generics

---
+++ tschap15/webapp/webpack.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    mode: "development",
+    entry: "./src/index.ts",
+    output: { filename: "bundle.js" },
+    resolve: { extensions: [".ts",".js"] },
+    module: {
+        rules: [
+            { test: /\.ts/, use: "ts-loader", exclude: /node_modules/ }
+        ]
+    }
+};
