97789bb12fbe3d6ce7062bb6e6fdf9b91b27eb49
caverdude <javacaverdude@gmail.com>
2020-10-19 23:14:45 -0500

    Chapter 13 advanced generics

--- tschap13/dist/index.js
+++ tschap13/dist/index.js
@@ -1,14 +1,10 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const dataTypes_1 = require("./dataTypes");
-function getValue(data, propName) {
-    if (Array.isArray(data)) {
-        return data[0][propName];
-    }
-    else {
-        return data[propName];
-    }
+function processArray(data, func) {
+    return data.map(item => func(item));
 }
+let selectName = (p) => p.name;
 let products = [new dataTypes_1.Product("Kayak", 275), new dataTypes_1.Product("Lifejacket", 48.95)];
-console.log(`Array Value: ${getValue(products, "price")}`);
-console.log(`Single Total: ${getValue(products[0], "price")}`);
+let names = processArray(products, selectName);
+names.forEach(name => console.log(`Name: ${name}`));
