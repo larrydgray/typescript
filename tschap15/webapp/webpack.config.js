c7a681e28cf547e15c5a5baee8d1a37dcc5dc751
caverdude <javacaverdude@gmail.com>
2020-10-23 14:33:54 -0500

    Chapter 15 ts web app

--- tschap15/webapp/webpack.config.js
+++ tschap15/webapp/webpack.config.js
@@ -2,7 +2,7 @@ module.exports = {
     mode: "development",
     entry: "./src/index.ts",
     output: { filename: "bundle.js" },
-    resolve: { extensions: [".ts","tsx", ".js",".css"] },
+    resolve: { extensions: [".ts",".tsx", ".js",".css"] },
     module: {
         rules: [
             { test: /\.tsx?$/, use: "ts-loader", exclude: /node_modules/ },
