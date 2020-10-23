e7ddac4ce0bd70f637aeafe1111faa64df0127e6
caverdude <javacaverdude@gmail.com>
2020-10-22 21:40:57 -0500

    Chapter 15 ts web app

---
+++ tschap15/webapp/src/htmlDisplay.tsx
@@ -0,0 +1,62 @@
+//import { createElement } from "./tools/jsxFactory";
+import { Product, Order } from "./data/entities";
+//import { AbstractDataSource } from "./data/abstractDataSource";
+//import { ProductList } from "./productList.ts.rem";
+
+export class HtmlDisplay {
+    props: {
+        products: Product[],
+        order: Order
+    }
+    getContent(): HTMLElement {
+       return <h3 className={"bg-secondary text-center text-white p-2"}>
+                    {this.getElementText() }
+              </h3>
+    }
+
+    getElementText() {
+        return `${this.props.products.length} Products, `
+            + `Order total: $${ this.props.order.total }`;
+    }
+
+
+    /*private containerElem: HTMLElement;
+    private selectedCategory: string;
+
+    constructor() {
+        this.containerElem = document.createElement("div");
+    }
+
+    props: {
+        dataSource: AbstractDataSource;
+    }
+
+    async getContent(): Promise<HTMLElement> {
+        await this.updateContent();
+        return this.containerElem;
+    }
+
+    async updateContent() {
+        let products = await this.props.dataSource.getProducts("id",
+            this.selectedCategory);
+        let categories = await this.props.dataSource.getCategories();
+        this.containerElem.innerHTML = "";
+        let content = <div>
+            <ProductList products={ products } categories={ categories }
+                         selectedCategory={ this.selectedCategory }
+                         addToOrderCallback={ this.addToOrder }
+                         filterCallback={ this.selectCategory} />
+        </div>
+        this.containerElem.appendChild(content);
+    }
+
+    addToOrder = (product: Product, quantity: number) => {
+        this.props.dataSource.order.addProduct(product, quantity);
+        this.updateContent();
+    }
+
+    selectCategory = (selected: string) => {
+        this.selectedCategory = selected === "All" ? undefined : selected;
+        this.updateContent();
+    }*/
+}
