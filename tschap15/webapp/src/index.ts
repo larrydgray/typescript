9b7b69464eb7fd68bcbd2754b87596d1c3d94b2b
caverdude <javacaverdude@gmail.com>
2020-10-22 20:36:05 -0500

    Chapter 15 ts web app

--- tschap15/webapp/src/index.ts
+++ tschap15/webapp/src/index.ts
@@ -1,39 +1,27 @@
 import { LocalDataSource } from "./data/localDataSource";
+import { DomDisplay} from "./domDisplay";
 //import { HtmlDisplay } from "./htmlDisplay";
-//import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/css/bootstrap.css";
 
-async function displayData(): Promise<string> {
-    let ds = new LocalDataSource();
-    let allProducts = await ds.getProducts("name");
-    let categories = await ds.getCategories();
-    let chessProducts = await ds.getProducts("name", "Chess");
-    console.log(chessProducts.length);
-    let result = "";
-    allProducts.forEach(p => result += `Product: ${p.name}, ${p.category}, ${p.price}\n`);
-    categories.forEach(c => result += (`Category: ${c}\n`));
-    chessProducts.forEach(p=> ds.order.addProduct(p,1));
-    result+=`Order total: $${ds.order.total.toFixed(2)}`;
-    return result;
+let ds = new LocalDataSource();
+
+async function displayData(): Promise<HTMLElement> {
+    let display = new DomDisplay();
+
+    display.props = {
+        products: await ds.getProducts("name"),
+        order: ds.order
+    }
+
+    return display.getContent();
 }
 
-//let ds = new LocalDataSource();
-
-/*function displayData(): Promise<HTMLElement> {
-    let display = new HtmlDisplay();
-    display.props = {
-        dataSource: ds
-    }
-    return display.getContent();
-}*/
-
-displayData().then( res => console.log(res));
-/*
 document.onreadystatechange = () => {
-    if (document.readyState === "complete") {
+    if(document.readyState === "complete"){
         displayData().then(elem => {
             let rootElement = document.getElementById("app");
             rootElement.innerHTML = "";
             rootElement.appendChild(elem);
-        });
+        })
     }
-};*/
+}
