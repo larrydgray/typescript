d64e0a8394caada2b8e46cc4a59a087c9d38db99
caverdude <javacaverdude@gmail.com>
2020-10-23 15:11:25 -0500

    Chapter 15 ts web app

--- tschap15/webapp/src/htmlDisplay.tsx
+++ tschap15/webapp/src/htmlDisplay.tsx
@@ -1,26 +1,10 @@
 import { createElement } from "./tools/jsxFactory";
 import { Product, Order } from "./data/entities";
-//import { AbstractDataSource } from "./data/abstractDataSource";
-//import { ProductList } from "./productList.tsx";
+import { AbstractDataSource } from "./data/abstractDataSource";
+import { ProductList } from "./productList";
 
 export class HtmlDisplay {
-    props: {
-        products: Product[],
-        order: Order
-    }
-    getContent(): HTMLElement {
-       return <h3 className={"bg-secondary text-center text-white p-2"}>
-                    {this.getElementText() }
-              </h3>
-    }
-
-    getElementText() {
-        return `${this.props.products.length} Products, `
-            + `Order total: $${ this.props.order.total }`;
-    }
-
-
-    /*private containerElem: HTMLElement;
+    private containerElem: HTMLElement;
     private selectedCategory: string;
 
     constructor() {
@@ -58,5 +42,5 @@ export class HtmlDisplay {
     selectCategory = (selected: string) => {
         this.selectedCategory = selected === "All" ? undefined : selected;
         this.updateContent();
-    }*/
+    }
 }
--- tschap15/webapp/src/index.ts
+++ tschap15/webapp/src/index.ts
@@ -9,8 +9,7 @@ async function displayData(): Promise<HTMLElement> {
     let display = new HtmlDisplay();
 
     display.props = {
-        products: await ds.getProducts("name"),
-        order: ds.order
+        dataSource: ds
     }
 
     return display.getContent();
