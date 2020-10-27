32fb4d952ea2ff0c24dfb1038b96f6c65a35b8ea
caverdude <javacaverdude@gmail.com>
2020-10-26 22:39:01 -0500

    Chapter 17 angular8

--- tschap17/angular8/angularapp/src/app/app.module.ts
+++ tschap17/angular8/angularapp/src/app/app.module.ts
@@ -8,10 +8,21 @@ import { ProductItem } from './productItem.component';
 import { CategegoryList } from "./categoryList.component";
 import { Header } from "./header.component";
 import { ProductList } from "./productList.component";
+import { RouterModule } from "@angular/router"
+import { OrderDetails } from "./orderDetails.component";
+import { Summary } from "./summary.component";
+
+const routes = RouterModule.forRoot([
+  { path: "products", component: ProductList },
+  { path: "order", component: OrderDetails},
+  { path: "summary/:id", component: Summary},
+  { path: "", redirectTo: "/products", pathMatch: "full"}
+]);
 
 @NgModule({
-  declarations: [AppComponent, ProductItem, CategegoryList, Header, ProductList],
-  imports: [BrowserModule, AppRoutingModule, FormsModule, DataModelModule],
+  declarations: [AppComponent,  ProductItem, CategegoryList, Header, ProductList,
+    OrderDetails, Summary],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, DataModelModule, routes],
   providers: [],
   bootstrap: [AppComponent]
 })
