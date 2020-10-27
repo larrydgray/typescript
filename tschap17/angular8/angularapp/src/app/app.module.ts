dbc1caf6512b7bf3755909486160ee106141377d
caverdude <javacaverdude@gmail.com>
2020-10-26 22:13:02 -0500

    Chapter 17 angular8

--- tschap17/angular8/angularapp/src/app/app.module.ts
+++ tschap17/angular8/angularapp/src/app/app.module.ts
@@ -1,19 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpClientModule} from "@angular/common/http";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule } from "@angular/forms";
+import { DataModelModule } from "./data/data.module";
+import { ProductItem } from './productItem.component';
+import { CategegoryList } from "./categoryList.component";
+import { Header } from "./header.component";
+import { ProductList } from "./productList.component";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule
-  ],
+  declarations: [AppComponent, ProductItem, CategegoryList, Header, ProductList],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, DataModelModule],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
