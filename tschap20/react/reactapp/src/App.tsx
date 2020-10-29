6078f5d0f1f5cb528d64d83fd70b71c0ffb0d9d1
caverdude <javacaverdude@gmail.com>
2020-10-29 17:40:10 -0500

    Chapter 19 TypeScript with React

---
+++ tschap20/react/reactapp/src/App.tsx
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+//import { Product, Order } from './data/entities';
+//import { ProductList } from './productList';
+import { dataStore } from "./data/dataStore";
+import { Provider } from 'react-redux';
+import { HttpHandler } from "./data/httpHandler";
+import { addProduct } from './data/actionCreators';
+import { ConnectedProductList } from './data/productListConnector';
+import { Switch, Route, Redirect, BrowserRouter} from "react-router-dom";
+
+interface Props {
+    // no props required
+}
+
+export default class App extends Component<Props> {
+    private httpHandler = new HttpHandler();
+
+    constructor(props: Props) {
+        super(props);
+        this.httpHandler.loadProducts(data =>
+            dataStore.dispatch(addProduct(...data)));
+    }
+
+    render = () =>
+        <div className="App">
+            <Provider store={ dataStore }>
+                <BrowserRouter>
+                    <Route path="/products" component={ConnectedProductList}/>
+                    <Redirect to= "/Product"/>
+                </BrowserRouter>
+
+            </Provider>
+        </div>
+
+    submitCallback = () => {
+        console.log("Submit order");
+    }
+}
